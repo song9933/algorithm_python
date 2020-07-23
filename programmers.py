75f521a80fec9ac411017826d4c34d59f33c91af
SONG JOOYOUNG <sngbrm@gmail.com>
2020-07-23 08:25:22 +0000

    Programmers - 약수의합

--- programmers.py
+++ programmers.py
@@ -23,3 +23,11 @@ def sol_3(n):
     else :
         answer = -1
     return answer  
+
+#약수의 합
+def sol_4(n):
+  answer = 0
+  for i in range(1, n+1):
+    if n%i ==0:
+      answer += i
+  return answer
